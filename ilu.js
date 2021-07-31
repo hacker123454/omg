099217c45bacd5db37229576364988749f96620e
hacker123454 <84779573+hacker123454@users.noreply.github.com>
2021-08-01 02:58:09 +0300

    Update ilu.js

--- ilu.js
+++ ilu.js
@@ -1,7 +1,5 @@
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-  // true for mobile device
-  <meta http-equiv="refresh" content="0.1; URL=https://hacker123454.github.io/omg/omg.html" />
-}else{
-  // false for not mobile device
- <meta http-equiv="refresh" content="0.1; URL=https://hacker123454.github.io/omg/aaaaa.html" />
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    <meta http-equiv="refresh" content="0.1; URL=https://hacker123454.github.io/omg/omg.html" />
+      else{
+<meta http-equiv="refresh" content="0.1; URL=https://hacker123454.github.io/omg/aaaaa.html" />
 }

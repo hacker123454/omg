df09752d9e2c111db5d0f032689d96c40e595999
hacker123454 <84779573+hacker123454@users.noreply.github.com>
2021-08-01 02:56:23 +0300

    Update and rename ilu.html to ilu.js

---
+++ ilu.js
@@ -0,0 +1,7 @@
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  // true for mobile device
+  <meta http-equiv="refresh" content="0.1; URL=https://hacker123454.github.io/omg/omg.html" />
+}else{
+  // false for not mobile device
+ <meta http-equiv="refresh" content="0.1; URL=https://hacker123454.github.io/omg/aaaaa.html" />
+}

